026105a83118a379280ff39252d0dd19bcbafe0b
Maria <mashero4ka@gmail.com>
2020-11-06 22:05:26 +0200

    task working

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import './sass/main.scss'
+
+import colors from './js/colors'
+
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector("[data-action='stop']");
+
+let interval;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const onSwitch = () => {
+  body.style.background = colors[randomIntegerFromInterval(0, colors.length - 1)];
+}
+
+const onChangeStart = () => {
+  stopBtn.addEventListener('click', onChangeStop, { once: true });
+  interval = setInterval(onSwitch, 1000);
+}
+
+const onChangeStop = () => {
+  clearInterval(interval);
+  startBtn.addEventListener('click', onChangeStart, { once: true });
+}
+
+startBtn.addEventListener('click', onChangeStart, { once: true });
